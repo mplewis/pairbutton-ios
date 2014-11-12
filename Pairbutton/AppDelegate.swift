5219ebb8cb4943d033d142b79e47ba8146b4eb85
Matthew Lewis <matt@mplewis.com>
2014-11-12 05:58:37 -0600

    Initial Commit

---
+++ Pairbutton/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pairbutton
+//
+//  Created by Matthew Lewis on 11/12/14.
+//  Copyright (c) 2014 Kestrel Development. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pairbutton/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pairbutton
+//
+//  Created by Matthew Lewis on 11/12/14.
+//  Copyright (c) 2014 Kestrel Development. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
